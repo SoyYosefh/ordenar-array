551720cdfb7b31ea8ff99d8b6b1a4f965611fc54
SoyYosefh <joratejedana@gmail.com>
2024-08-28 17:47:50 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// 4. Crea un arreglo de números desordenados y luego 
+// ordénalo de menor a mayor utilizando algún algoritmo 
+// de ordenamiento (p. ej., el método sort()).
+
+const numeros = [5, 3, 8, 1, 2, 9, 4, 7, 6];
+console.log(`Números desordenados: ${numeros}`);
+const numerosOrdenados = numeros.sort();
+console.log(`Números ordenados: ${numerosOrdenados}`);
